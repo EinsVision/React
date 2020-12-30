58ba63e8f2fae49654d34f6524e7b9f84887d603
jungchangdae <einsvision@gmail.com>
2020-12-30 17:02:48 +0900

    Component 만들기

--- src/App.js
+++ src/App.js
@@ -1,15 +1,52 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
 import './App.css';
+
+class Subject extends Component{
+  render(){
+    return(
+      <header>
+        <h1>WEB</h1>
+        <div>world wide web!</div>
+      </header>
+    );
+  }
+}
+
+class Navigation extends Component{
+  render(){
+    return(
+      <nav>
+        <ul>
+            <li ><a href="1.html">HTML</a></li>
+            <li ><a href="2.html">CSS</a></li>
+            <li ><a href="3.html">JS</a></li>
+        </ul>
+      </nav>
+    );
+  }
+}
+
+class Article extends Component{
+  render(){
+    return(
+      <article>
+        <h2>HTML</h2>
+        <div>HTML is HyperText Markup Language.</div>
+      </article>
+    );
+  }
+}
 
 class App extends Component {
   render () {
     return (
-    <div className="App">
-      Hello React!! from EinsVision
-    </div>
-  );
-}
+      <div className="App">
+        <Subject></Subject>
+        <Navigation></Navigation>
+        <Article></Article>
+      </div>
+    );
+  }
 }
 
 export default App;
