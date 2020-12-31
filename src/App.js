0dbe226dda09c06acc99b5b6272e6e660a9f57a7
jungchangdae <einsvision@gmail.com>
2020-12-31 23:28:08 +0900

    State 사용

--- src/App.js
+++ src/App.js
@@ -5,10 +5,20 @@ import Article from "./components/Article"
 import './App.css';
 
 class App extends Component {
+  // component가 실행될 때 redner 함수보다 먼저 실행이 되면서
+  // 그 component를 초기화해주는 code는 constructor안에 쓴다. 
+  constructor(props){
+    super(props);
+    this.state = {
+      subject:{title:'WEB', sub:'World wide web!'}
+    }
+  }
   render () {
     return (
       <div className="App">
-        <Subject title="WEB" sub="world wide web!"></Subject>
+        <Subject title={this.state.subject.title} 
+          sub={this.state.subject.sub}>
+        </Subject>
         <Subject title="React" sub="world best React!"></Subject>
         <Navigation></Navigation>
         <Article title="HTML" desc="HTML is HyperText Markup Language."></Article>
