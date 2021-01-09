b482c810f05da848fe2f15aa44da8be24aeed745
jungchangdae <einsvision@gmail.com>
2021-01-09 14:37:26 +0900

    JSX, PROPS 개념

--- src/App.js
+++ src/App.js
@@ -1,7 +1,70 @@
 import React from 'react';
 
+/*
+ 아래 두개는 같은 것이다.
+ props.fav
+ { fav }
+*/
+
+function Food({fav}){
+  // console.log(props);
+  return(
+    <h3> I like {fav}</h3>
+  ); 
+}
+
+const foodILike = [
+  {
+    name: 'Kimchi',
+    image: 'https://admin.cjrecipe.com:9007/images/theKitchen/PHON/0000001651/0000006094/0000001651.jpg'
+  },
+  {
+    name: 'Samgyeopsal',
+    image: 'http://cdn.011st.com/11dims/resize/600x600/quality/75/11src/pd/21/6/2/3/3/6/4/fMusd/2884623364_B.jpg'
+  },
+  {
+    name: 'ramen',
+    image: 'https://www.newiki.net/wiki/%ED%8C%8C%EC%9D%BC:Instant_ramens_in_a_korean_supermarket.jpg'
+  },
+  {
+    name: 'Kimchi',
+    image: 'https://admin.cjrecipe.com:9007/images/theKitchen/PHON/0000001651/0000006094/0000001651.jpg'
+  },
+  {
+    name: 'Kimchi',
+    image: 'https://admin.cjrecipe.com:9007/images/theKitchen/PHON/0000001651/0000006094/0000001651.jpg'
+  },
+  
+
+];
+
+
 function App(){
-  return <div><p>Hello potatos</p></div>;
+  return (
+  <div>
+    <p>Hello potatos</p>
+    <Food
+      fav='kimchi'
+      something={true}
+      papapapa={['hello',1,2,3,4,true]} 
+    />
+    <Food
+      fav='ramen'
+      something={true}
+      papapapa={['hello',1,2,3,4,true]} 
+    />
+    <Food
+      fav='snack'
+      something={true}
+      papapapa={['hello',1,2,3,4,true]} 
+    />
+    <Food
+      fav='rice'
+      something={true}
+      papapapa={['hello',1,2,3,4,true]} 
+    />
+  </div>
+  );
 }
 
 export default App;
--- src/index.js
+++ src/index.js
@@ -8,4 +8,11 @@ ReactDOM.render(
   </React.StrictMode>,
   document.getElementById('root')
 );
- 
+
+// component는 HTML을 반환하는 함수이다.
+// React는 component를 사용해서 HTML처럼 작성하려는 경우에 필요하다.
+// javascript와 HTML 사이의 이러한 조합을 jsx라고 부른다.
+
+// react application은 한 번에 하나의 component만 rendering 할 수 있다는 점이야.
+// react가 멋진 이유는 재사용 가능한 component를 만들 수 있다는 점이다. 
+// 또한 component에서 component로 정보를 보낼 수 있다.
