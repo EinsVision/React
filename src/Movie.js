c21d4d9ae634028b181225fe239a99a050546eb1
jungchangdae <einsvision@gmail.com>
2021-01-11 14:08:53 +0900

    css 작업

--- src/Movie.js
+++ src/Movie.js
@@ -2,14 +2,14 @@ import React from 'react';
 import propTypes from 'prop-types';
 import './Movie.css'
 
-function Movie({id, year, title, summary, poster, genres}){
+function Movie({year, title, summary, poster, genres}){
     return (
     <div className='movie'>
         <img src={poster} alt={title} title={title}/>
         <div className='movie_data'>
             <h3 className='movie_title'>{title}</h3>
             <h5 className='movie_year'>{year}</h5>
-            <ul className='genres'>
+            <ul className='movie_genres'>
                 {genres.map((genres, index) => (
                     <li key={index} className='genres_genre'>{genres}</li>
                 ))}
